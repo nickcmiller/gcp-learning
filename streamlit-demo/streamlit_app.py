a90b41406739ad51eabe5e6e413464e087de4d37
Nick Miller <miller.nick.c@gmail.com>
2024-05-16 13:53:17 -0700

    Change to Llama 70B

--- streamlit-demo/streamlit_app.py
+++ streamlit-demo/streamlit_app.py
@@ -14,7 +14,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 def get_groq_response(messages: list):
     try:
         response = client.chat.completions.create(
-            model="llama3-8b-8192",
+            model="llama3-70b-8192",
             messages=messages
         )
         return response.choices[0].message.content
